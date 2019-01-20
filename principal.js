d6e4c782eee545cb3e122832087c9a7dae02e75b
wagneribanez2 <wagneribanez2@hotmail.com>
2019-01-20 13:29:37 -0200

    Alterando textos do banner

--- principal.js
+++ principal.js
@@ -1,11 +1,10 @@
-var banners =
-["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var banners = ["Do lixo ao luxo!", "Reaproveitar é aproveitar!"];
 var bannerAtual = 0;
 
 function trocaBanner() {
     bannerAtual = (bannerAtual + 1) % 2;
     document.querySelector(’h2#mensagem’).textContent =
-    banners[bannerAtual];
-    }
+        banners[bannerAtual];
+}
 
-    setInterval(trocaBanner, 1000);
+setInterval(trocaBanner, 1000);
